9484b17ad29a1a7037516583e44ee66d5123e870
TiboJhjj <jhjjhjkhj@gmail.com>
2023-05-04 10:58:15 +0200

    ajout fonctions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import "./App.css";
 import React from "react";
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Notes from "./components/Notes";
 import Navbar from "./components/Navbar";
 
 function App() {
+    const [notes, setNotes] = useState(null);
+
+    const fetchNotes = async () => {
+        const response = await fetch("/notes");
+        const data = await response.json();
+        setNotes(data);
+    };
+
+    const addNote = async (note) => {
+        const response = await fetch("/notes", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(note),
+        });
+        const data = await response.json();
+        setNotes([...notes, data]);
+    };
+
+    const deleteNote = async (id) => {
+        await fetch(`/notes/${id}`, {
+            method: "DELETE",
+        });
+        setNotes(notes.filter((note) => note.id !== id));
+    };
+
+    const updateNote = async (id, note) => {
+        const response = await fetch(`/notes/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(note),
+        });
+        const data = await response.json();
+        setNotes(
+            notes.map((note) => (note.id === id ? { ...note, ...data } : note))
+        );
+    };
     return (
         <div className="App">
             <Navbar />
